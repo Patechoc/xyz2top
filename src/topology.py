0726da7cd808daad2cf4a06d3d3b9e3adfe06d69
Patrick Merlot <patrick.merlot@gmail.com>
2015-04-08 23:51:49 +0200

    forgot topology.py

---
+++ src/topology.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python                                                                              
+
+import sys, os
+import argparse
+import numpy as np
+
+
+def main():
+    # parse_molecule_XYZ()
+    # get_atoms()
+    # build_matrix_distance()
+    # detect_covalent_bonds() # build unique connected pairs and add connectedAtoms to each atom 
+    # get_angles()
+    # get_dihedral_angles()
+    # print_topology()
+
+if __name__ == "__main__":
+    main()
