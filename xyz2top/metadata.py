b73a5033f4227070fe438e347a2fd8ef1f4c8fe0
Patrick Merlot <patrick.merlot@gmail.com>
2015-04-08 10:46:01 +0200

    forgot to push

---
+++ xyz2top/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project xyz2top
+
+generates topology file from a geometry in xyz format
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'xyz2top'
+project = 'XYZ to TOP'
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = "Topology From Geometry In XYZ Format"
+authors = ['Patrick Merlot']
+authors_string = ', '.join(authors)
+emails = ['patrick.merlot@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/Patechoc/xyz2top'
