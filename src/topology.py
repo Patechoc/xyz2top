af74242c614ec4c9a963bdff32708e3e4443db52
Patrick Merlot <patrick.merlot@gmail.com>
2015-04-14 15:12:39 +0200

    store neighbours per atom, needed to find angles afterward. Can't print the list of neighbours, just their number

--- src/topology.py
+++ src/topology.py
@@ -8,30 +8,46 @@ import math
 import atomInMolecule as mol
 import elements
 
+class atomEntity(object):
+    def __init__(self, atomObject, atomIndex):
+        self.atomIndex     = atomIndex
+        self.atomObject    = atomObject
+        self.indicesNeighbours = []
+    def add_neighbourAtom(self, atomEntityObject):
+        self.indicesNeighbours.append(atomEntityObject.atomIndex)
+    def __str__(self):
+        str = "ATOM ENTITY: index={}, infos= {}".format(self.atomIndex,self.atomObject)
+        if len(self.indicesNeighbours) > 0:
+            #str += ", #neighbours= {} ({})".format(len(self.indicesNeighbours),",".join([val for val in self.indicesNeighbours]))
+            str += ", #neighbours= {} ()".format(len(self.indicesNeighbours))
+        else:
+            str += ", #neighbours= 0"
+        return str
+
 class atomPair(object):
-    def __init__(self, atom_i, atom_j, i, j):
-        self.atom_i   = atom_i
-        self.atom_j   = atom_j
-        self.index_ai = i
-        self.index_aj = j
+    def __init__(self, atomEntity_i, atomEntity_j):
+        self.atomEntity_i = atomEntity_i
+        self.atomEntity_j = atomEntity_j
         # INTER-ATOMIC DISTANCE
-        self.distance = get_atomic_separation(atom_i, atom_j)
+        self.distance = get_atomic_separation(self.atomEntity_i.atomObject,
+                                            self.atomEntity_j.atomObject)
         # COVALENT BOND DISTANCE
         # http://chemwiki.ucdavis.edu/Theoretical_Chemistry/Chemical_Bonding/General_Principles/Covalent_Bond_Distance,_Radius_and_van_der_Waals_Radius
         self.covDist = self.sum_covalent_radii()
     def sum_covalent_radii(self):
-        ele_ai = elements.ELEMENTS[self.atom_i.atomSymbol]
-        ele_aj = elements.ELEMENTS[self.atom_j.atomSymbol]
+        ele_ai = elements.ELEMENTS[self.atomEntity_i.atomObject.atomSymbol]
+        ele_aj = elements.ELEMENTS[self.atomEntity_j.atomObject.atomSymbol]
         covRad_ai = ele_ai.covrad
         covRad_aj = ele_aj.covrad
         return covRad_ai + covRad_aj
     def __str__(self):
-        return " ATOM PAIR indices: ({},{})\n\
+        return "ATOM PAIR between:\n\
         \t {}\n\
         \t {}\n\
         \t Covalent bond distance: {}\n\
         \t Interatomic distance : {}\
-        ".format(self.index_ai,self.index_aj,self.atom_i,self.atom_j,self.covDist, self.distance)
+        ".format(self.atomEntity_i,  self.atomEntity_j,
+                 self.covDist, self.distance)
 
 
 class topology(object):
@@ -43,22 +59,27 @@ class topology(object):
         self.covRadFactor = covRadFactor
         self.atomicPairs = [] # contains all atomPairs
         self.connections = [] # contains only atomPairs detected as connected
-
+        self.atomNeighbours = [atomEntity(ai,i) for i,ai in enumerate(self.molecule.listAtoms)]
     def is_connected(self, pair):
         isConnected = False
-        if ( get_atomic_separation(pair.atom_i, pair.atom_j)
+        if ( get_atomic_separation(pair.atomEntity_i.atomObject,
+                                   pair.atomEntity_j.atomObject)
              < self.covRadFactor * pair.covDist):
             isConnected = True
         return isConnected
 
     def check_connection(self, ai, aj, i, j):
-        pair = atomPair(ai, aj, i, j)
+        entity_i = atomEntity(ai, i)
+        entity_j = atomEntity(aj, j)
+        pair = atomPair(entity_i, entity_j)
         # add the pair to the list of atomic pairs
         self.atomicPairs.append(pair)
         # if the atoms are 'close' enough, add the pair to the list of connections too
         if self.is_connected(pair):
             self.connections.append(pair)
-            #print pair
+            self.atomNeighbours[i].add_neighbourAtom(entity_j)
+            self.atomNeighbours[j].add_neighbourAtom(entity_i)
+            print atomPair(self.atomNeighbours[i], self.atomNeighbours[j])
         #print pair
 
     def get_atomicConnections(self):
