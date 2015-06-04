96d46d503de5f27edb049e8ff70919ee7497c5bb
Patrick Merlot <patrick.merlot@gmail.com>
2015-06-04 14:20:22 +0200

    small script to convert optimized LSDALTON geometries to XYZ format and the outputs

--- xyz2top/tests/files/optimGeometries_to_xyz.py
+++ xyz2top/tests/files/optimGeometries_to_xyz.py
@@ -2,9 +2,24 @@
 
 import read_LSDALTON_output as readLS
 
-path = "/home/ctcc2/Documents/CODE-DEV/xyz2top/xyz2top/tests/files"
+filenames = ["valinomycin_geomOpt_DFT-b3lyp_6-31Gs_ADMM2-KT3X_3-21G.out",
+             "valinomycin_geomOpt_DFT-b3lyp_6-31Gs_ADMM2-PBEX_3-21G.out",
+#             "valinomycin_geomOpt_DFT-b3lyp_6-31Gs_noDF.out",
+             "valinomycin_geomOpt_DFT-b3lyp_6-31Gs.out",
+             "valinomycin_geomOpt_DFT-b3lyp_cc-pVTZ_ADMM2-KT3X_3-21G.out",
+             "valinomycin_geomOpt_DFT-b3lyp_cc-pVTZ_ADMM2-PBEX_3-21G.out",
+             "valinomycin_geomOpt_DFT-b3lyp_cc-pVTZ_noDF.out",
+             "valinomycin_geomOpt_DFT-b3lyp_cc-pVTZ.out"]
+
+path = "/home/ctcc2/Documents/CODE-DEV/xyz2top/xyz2top/tests/files/"
 filename = "valinomycin_geomOpt_DFT-b3lyp_cc-pVTZ.out"
-str = readLS.get_optmized_MOL_string(path + filename)
+
+for filename in filenames:
+    molOptimized = readLS.parse_molecule_optimized(path + filename)
+    xyz = molOptimized.getContent_format_XYZ()
+    #    print xyz
+    with open(filename+".xyz", 'w') as outfile:
+        outfile.write(xyz)
 
 # valinomycin_geomOpt_DFT-b3lyp_6-31Gs_ADMM2-KT3X_3-21G.out
 # valinomycin_geomOpt_DFT-b3lyp_6-31Gs_ADMM2-PBEX_3-21G.out
